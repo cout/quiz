20c9df4bd812f95cbabcf4b0161a576733669275
Paul Brannan <curlypaul924@gmail.com>
2010-11-11 12:49:39 -0500

    Added help in the case of incorrect cmdline usage.

--- quiz.rb
+++ quiz.rb
@@ -1,7 +1,17 @@
 require 'quiz'
 
 #definitions = Definitions.load('test.txt')
-definitions = Definitions.load('biology_ch_9.txt')
+#definitions = Definitions.load('biology_ch_9.txt')
+# ARGV.each do |arg|
+  # definitions = Definitions.load(arg)
+  # end
+
+if not ARGV[0] then
+  puts "Usage: #$0 <filename>"
+  exit 1
+end
+
+definitions = Definitions.load(ARGV[0])
 quiz = MultipleChoiceQuiz.new(definitions)
 quiz.go
 
