aa6791366bd0c22b76ea82ab270dc83ae1cdfe91
Paul Brannan <curlypaul924@gmail.com>
2010-12-04 13:44:26 -0500

    More simplification

--- lib/quiz/quiz.rb
+++ lib/quiz/quiz.rb
@@ -14,10 +14,6 @@ class Quiz
   end
 
   def got_response(question, response)
-    validate_response(question, response)
-  end
-
-  def validate_response(question, response)
     if response == question.correct_response then
       correct_response(question, response)
     else
@@ -33,9 +29,6 @@ class Quiz
   def incorrect_response(question, response)
     @frontend.incorrect_response(question)
     record_response(question, response, false)
-  end
-
-  def record_response(question, response, was_correct)
   end
 
   def ask
