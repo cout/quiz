e1bb93dfaeda82fa3ba008270337ec3281880e46
Paul Brannan <curlypaul924@gmail.com>
2010-12-04 13:32:46 -0500

    Refactored quiz logic into base class.

--- lib/quiz/multiple_choice_quiz.rb
+++ lib/quiz/multiple_choice_quiz.rb
@@ -1,8 +1,4 @@
 require 'quiz/multiple_choice_question'
-require 'quiz/stats'
-require 'quiz/encouragement'
-
-require 'set'
 
 module Quiz
 
@@ -31,63 +27,23 @@ class MultipleChoiceQuestionGenerator
 
     return choices.shuffle
   end
-
 end
 
 class MultipleChoiceQuiz < Quiz
   def initialize(frontend, *definitions)
-    super(frontend)
+    generators = create_generators(*definitions)
 
-    @generators = []
-    @ask_more = Set.new
-    @stats = {}
-
-    load_definitions(*definitions)
+    super(frontend, *generators)
   end
 
-  def load_definitions(*definitions_set)
+  def create_generators(*definitions_set)
+    generators = []
     definitions_set.each do |definitions|
       definitions.each do |definition|
-        @generators << MultipleChoiceQuestionGenerator.new(definition)
+        generators << MultipleChoiceQuestionGenerator.new(definition)
       end
     end
-  end
-
-  def ask(n=4)
-    # TODO: if a particular set has fewer questions, those questions
-    # will be presented more often than those from sets with more
-    # questions
-
-    generator = self.pick_question_generator()
-    question = generator.generate_question(n)
-
-    return question
-  end
-
-  def pick_question_generator()
-    if rand(2) == 0 and @ask_more.length > 0 then
-      ask_more = @ask_more.to_a.shuffle
-      return ask_more[0]
-    else
-      idx = rand(@generators.length)
-      return @generators[idx]
-    end
-  end
-
-  def record_response(question, response, was_correct)
-    generator = question.generator
-
-    stats = (@stats[question.correct_response] ||= Stats.new(question.correct_response))
-    stats.record_response(was_correct)
-
-    if stats.percent_correct > 0.50 then
-      @ask_more.delete(generator)
-    else
-      @ask_more.add(generator)
-    end
-
-    encouragement = Encouragement.after_response(stats, was_correct)
-    puts encouragement
+    return generators
   end
 end
 
--- lib/quiz/quiz.rb
+++ lib/quiz/quiz.rb
@@ -1,8 +1,16 @@
+require 'quiz/stats'
+require 'quiz/encouragement'
+
+require 'set'
+
 module Quiz
 
 class Quiz
-  def initialize(frontend)
+  def initialize(frontend, *generators)
     @frontend = frontend
+    @generators = generators
+    @ask_more = Set.new
+    @stats = {}
   end
 
   def got_response(question, response)
@@ -30,8 +38,41 @@ class Quiz
   def record_response(question, response, was_correct)
   end
 
-  def ask
-    raise NotImplementedError("ask() not implemented")
+  def ask(n=4)
+    # TODO: if a particular set has fewer questions, those questions
+    # will be presented more often than those from sets with more
+    # questions
+
+    generator = self.pick_question_generator()
+    question = generator.generate_question(n)
+
+    return question
+  end
+
+  def pick_question_generator()
+    if rand(2) == 0 and @ask_more.length > 0 then
+      ask_more = @ask_more.to_a.shuffle
+      return ask_more[0]
+    else
+      idx = rand(@generators.length)
+      return @generators[idx]
+    end
+  end
+
+  def record_response(question, response, was_correct)
+    generator = question.generator
+
+    stats = (@stats[question.correct_response] ||= Stats.new(question.correct_response))
+    stats.record_response(was_correct)
+
+    if stats.percent_correct > 0.50 then
+      @ask_more.delete(generator)
+    else
+      @ask_more.add(generator)
+    end
+
+    encouragement = Encouragement.after_response(stats, was_correct)
+    puts encouragement
   end
 end
 
